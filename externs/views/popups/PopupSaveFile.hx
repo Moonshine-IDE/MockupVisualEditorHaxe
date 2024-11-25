a03b9d1f8a152f65a9cd5b03b4532bf1ce58906d
rat-moonshine <rat-moonshine@prominic.net>
2024-11-25 19:43:24 +0530

    extern file update for https://github.com/Moonshine-IDE/moonshine-dev-private/issues/43

--- externs/views/popups/PopupSaveFile.hx
+++ externs/views/popups/PopupSaveFile.hx
@@ -2,9 +2,15 @@ package views.popups;
 
 import feathers.controls.TitleWindow;
 
+extern enum abstract SaveType(String)
+{
+    var MXHX;
+    var FormBuilderProject;
+    var FormBuilder;
+}
 extern class PopupSaveFile extends TitleWindow 
 {
-	public function new();
+	public function new(saveAsType:SaveType);
 	
     public var isCancelled(default, never):Bool;
     public var fileName(default, default):String;
